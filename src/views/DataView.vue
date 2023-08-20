ecd19cddbc1cbf0bfb2a1eea1d9543e93853e82c
jinweihuang20 <jinweihuang1020@gmail.com>
2023-08-20 13:25:17 +0800

    暫時隱藏稼動率頁面/調整Data頁面Tab順序

--- src/views/DataView.vue
+++ src/views/DataView.vue
@@ -1,23 +1,23 @@
 <template>
   <div class="data-view custom-tabs-head-large">
     <b-tabs size>
-      <b-tab title="AGV Locus">
+      <b-tab title="歷史任務查詢">
         <div class="w-100">
-          <AGVLocusDisplay></AGVLocusDisplay>
+          <TaskQuery></TaskQuery>
         </div>
       </b-tab>
-      <b-tab title="機台稼動">
-        <AvailbilitysData></AvailbilitysData>
-      </b-tab>
-      <b-tab title="警報查詢">
+      <b-tab title="歷史警報查詢">
         <div class="w-100">
           <AlarmQuery></AlarmQuery>
         </div>
       </b-tab>
-      <b-tab title="任務歷史查詢">
+      <b-tab title="AGV軌跡查詢">
         <div class="w-100">
-          <TaskQuery></TaskQuery>
+          <AGVLocusDisplay></AGVLocusDisplay>
         </div>
+      </b-tab>
+      <b-tab v-if="false" title="機台稼動">
+        <AvailbilitysData></AvailbilitysData>
       </b-tab>
     </b-tabs>
   </div>
