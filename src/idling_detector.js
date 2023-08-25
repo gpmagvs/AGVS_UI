4f6ae50e84b8f37bafd049c0634cc2e27b63c22f
gpm <gpm@gpmcorp.com.tw>
2023-08-25 17:59:16 +0800

    add idle detection function

---
+++ src/idling_detector.js
@@ -0,0 +1,20 @@
+import bus from './event-bus'
+var idleTime = 0
+var interval = setInterval(() => {
+    idleTime += 1;
+    if (idleTime >= 60) {
+        resetTimer()
+        location.reload()
+    }
+}, 1000);
+
+document.addEventListener('touchstart', resetTimer);
+document.addEventListener('touchmove', resetTimer);
+document.addEventListener('touchend', resetTimer);
+document.addEventListener('mousemove', resetTimer);
+document.addEventListener('mousedown', resetTimer)
+document.addEventListener('keydown', resetTimer);
+
+function resetTimer() {
+    idleTime = 0;
+}
