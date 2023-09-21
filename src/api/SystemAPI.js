23ac564a115432cac6fc9062840804950606d721
jinweihuang <jinweihuang2033@outlook.com>
2023-09-21 09:14:56 +0800

    閒置時間超出設定時進行登出/code clean

--- src/api/SystemAPI.js
+++ src/api/SystemAPI.js
@@ -1,13 +1,20 @@
 import axios from 'axios'
 import param from '@/gpm_param'
 var _axios = axios.create({
-  baseURL: param.backend_host,
+   baseURL: param.backend_host,
 })
+
+/**取得前端網頁設定參數 */
+export async function GetFrontendOptions() {
+   var ret = await _axios.get(`api/System/WebSite`);
+   return ret.data;
+}
+
 
 
 /**取得模式設定狀態(RUNMODE, HOST_CONN, HOST_OPERATION ) */
-export async function GetOperationStates(mode =0|1){
-   var ret= await _axios.get(`api/system/OperationStates`);
+export async function GetOperationStates(mode = 0 | 1) {
+   var ret = await _axios.get(`api/system/OperationStates`);
    return ret.data;
 }
 
@@ -16,8 +23,8 @@ export async function GetOperationStates(mode =0|1){
  * 0:Maintain
  * 1:Run
  */
-export async function RunMode(mode =0|1){
-   var ret= await _axios.post(`api/system/runmode?mode=${mode}`);
+export async function RunMode(mode = 0 | 1) {
+   var ret = await _axios.post(`api/system/runmode?mode=${mode}`);
    return ret.data;
 }
 
@@ -25,17 +32,17 @@ export async function RunMode(mode =0|1){
  * 0:Offline
  * 1:Online
  */
-export async function HostConnMode(mode =0|1){
-    var ret= await _axios.post(`api/system/hostconn?mode=${mode}`);
-    return ret.data;
- }
- 
+export async function HostConnMode(mode = 0 | 1) {
+   var ret = await _axios.post(`api/system/hostconn?mode=${mode}`);
+   return ret.data;
+}
+
 /**HOST Local/Remote模式切換。
  * 0:Local
  * 1:Remote
  */
-export async function HostOperationMode(mode =0|1){
-    var ret= await _axios.post(`api/system/hostoperation?mode=${mode}`);
-    return ret.data;
- }
+export async function HostOperationMode(mode = 0 | 1) {
+   var ret = await _axios.post(`api/system/hostoperation?mode=${mode}`);
+   return ret.data;
+}
 
