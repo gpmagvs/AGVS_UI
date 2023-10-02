6e04471443f67b4a0947a04201ba6f2a455d8662
gpm_3F_agvs <gpm@gpmcorp.com.tw>
2023-10-02 12:39:17 +0800

    bug fix

--- src/components/SystemSettings/UserManager.vue
+++ src/components/SystemSettings/UserManager.vue
@@ -38,7 +38,7 @@
         <el-table-column label="操作" min-width="120">
           <template #default="scope">
             <div>
-              <el-button :disabled="scope.row.Role == 2 || scope.row.Role == 3" @click="DeleteUser(scope.row)" type="danger">刪除</el-button>
+              <el-button @click="DeleteUser(scope.row)" type="danger">刪除</el-button>
             </div>
           </template>
         </el-table-column>
@@ -62,7 +62,7 @@
                 :label="role.label"></el-option>
             </el-select>
           </el-form-item>
-          <b-button @click="HandleAddNewUserClick" class="w-100" variant="primary">新增</b-button>
+          <b-button :disabled="NewUser.UserName == '' || NewUser.Password == ''" @click="HandleAddNewUserClick" class="w-100" variant="primary">新增</b-button>
         </el-form>
       </div>
     </el-dialog>
