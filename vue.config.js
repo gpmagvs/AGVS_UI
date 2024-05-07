8ba539d5b40159aad9d59361fbd71e3afe4ef006
jinweihuang20 <101879478+jinweihuang20@users.noreply.github.com>
2024-05-07 09:14:46 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+
+const isDevelopment = process.env.NODE_ENV === 'development';
+
 module.exports = defineConfig({
-  transpileDependencies: true,
+  transpileDependencies: [],
   outputDir: '../AGVSystem/wwwroot',
-  runtimeCompiler: true,
-})
+  runtimeCompiler: isDevelopment, // 開發模式為 true，否則為 false
+});
