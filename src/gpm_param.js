9a8766b6f25b2f778a90991b306bb3c3aee36e33
jinweihunag <jinweihuang@gpmcorp.com.tw>
2024-05-07 09:33:32 +0800

    update veu.config.js / gpm_param.js

--- src/gpm_param.js
+++ src/gpm_param.js
@@ -3,8 +3,8 @@ var param = {
   // backend_host: 'http://192.168.0.103:7025',
   get backend_host() {
     if (process.env.NODE_ENV == 'development') {
-      return 'http://127.0.0.1:5216'
       return 'http://192.168.0.1:5216'
+      return 'http://127.0.0.1:5216'
       return 'http://192.168.0.2:5216'
       return 'http://192.168.0.55:5216'
       return 'http://192.168.0.192:5216'
--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,14 @@ const { defineConfig } = require('@vue/cli-service');
 const isDevelopment = process.env.NODE_ENV === 'development';
 
 module.exports = defineConfig({
+  devServer: {
+    client: {
+      overlay: {
+        warnings: false, // 控制是否显示警告
+        errors: false // 控制是否显示错误
+      }
+    }
+  },
   transpileDependencies: [],
   outputDir: '../AGVSystem/wwwroot',
   runtimeCompiler: isDevelopment, // 開發模式為 true，否則為 false
