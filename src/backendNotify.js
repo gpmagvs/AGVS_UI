94a920d2270eb11e4b67b63081b88b34771174ca
jinweihuang20 <ss610349@gmail.com>
2024-05-21 16:06:54 +0800

    提醒用websocket斷線重連機制

--- src/backendNotify.js
+++ src/backendNotify.js
@@ -25,36 +25,77 @@ class NotifyMessage {
     }
 }
 
+function InitWSNotification(agvs = true, vms = true) {
 
-let agvsNotifyWs = new WebSocket(param.backend_ws_host + '/api/event');
-let vmsNotifyWs = new WebSocket(param.vms_ws_host + '/api/event');
-agvsNotifyWs.onmessage = (evt) => {
-    var data = evt.data;
-    let notify = new NotifyMessage()
-    Object.assign(notify, JSON.parse(data))
-    if (notify.message === 'Map-Point-Enabled-Property-Changed') {
-        bus.emit('Map-Point-Enabled-Property-Changed')
+    if (agvs) {
+
+        let agvsNotifyWs = new WebSocket(param.backend_ws_host + '/api/event');
+
+        agvsNotifyWs.onopen = (evt) => {
+            ElNotification.success({
+                message: 'Notification Connect to AGVS'
+            })
+        }
+        agvsNotifyWs.onmessage = (evt) => {
+            var data = evt.data;
+            let notify = new NotifyMessage()
+            Object.assign(notify, JSON.parse(data))
+            if (notify.message === 'Map-Point-Enabled-Property-Changed') {
+                bus.emit('Map-Point-Enabled-Property-Changed')
+            }
+            if (notify.show) {
+                ElNotification({
+                    title: "AGVSystem " + notify.evt,
+                    type: notify.typeStr,
+                    message: notify.message
+                })
+            }
+        }
+        agvsNotifyWs.onclose = (evt) => {
+            setTimeout(() => {
+
+                ElNotification.error({
+                    message: 'Notification Disconnect to AGVS'
+                })
+                InitWSNotification(true, false);
+            }, 1000);
+        }
     }
-    if (notify.show) {
-        ElNotification({
-            title: "AGVSystem " + notify.evt,
-            type: notify.typeStr,
-            message: notify.message
-        })
+
+    if (vms) {
+
+        let vmsNotifyWs = new WebSocket(param.vms_ws_host + '/api/event');
+
+        vmsNotifyWs.onopen = (evt) => {
+            ElNotification.success({
+                message: 'Notification Connect to VMS'
+            })
+        }
+        vmsNotifyWs.onmessage = (evt) => {
+            var data = evt.data;
+            let notify = new NotifyMessage()
+            Object.assign(notify, JSON.parse(data))
+            if (notify.message === 'Map-Point-Enabled-Property-Changed') {
+                bus.emit('Map-Point-Enabled-Property-Changed')
+            }
+            if (notify.show) {
+                ElNotification({
+                    title: "VMS " + notify.evt,
+                    type: notify.typeStr,
+                    message: notify.message
+                })
+            }
+        }
+        vmsNotifyWs.onclose = (evt) => {
+            setTimeout(() => {
+
+                InitWSNotification(false, true);
+                ElNotification.error({
+                    message: 'Notification Disconnect to VMS'
+                })
+            }, 1000);
+        }
     }
 }
-vmsNotifyWs.onmessage = (evt) => {
-    var data = evt.data;
-    let notify = new NotifyMessage()
-    Object.assign(notify, JSON.parse(data))
-    if (notify.message === 'Map-Point-Enabled-Property-Changed') {
-        bus.emit('Map-Point-Enabled-Property-Changed')
-    }
-    if (notify.show) {
-        ElNotification({
-            title: "VMS " + notify.evt,
-            type: notify.typeStr,
-            message: notify.message
-        })
-    }
-}
+
+InitWSNotification();
