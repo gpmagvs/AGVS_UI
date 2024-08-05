240e6da0aac1ae5a329b0e77c486db4e2e8df79a
PoJen <solanotsubasa@gmail.com>
2024-08-05 09:39:45 +0800

    1. WIP編輯畫面 正名Sensor位置的顯示名稱 2. WIP編輯畫面 Column Tag顯示的數值參考到ColumnTag變數

--- src/components/RacksStatusView/PortSensorEditView.vue
+++ src/components/RacksStatusView/PortSensorEditView.vue
@@ -2,21 +2,21 @@
     <div class="">
         <div class="rack-sensor-container d-flex justify-content-between px-3">
             <div>
-                <div class="sensor-name naeme-rack-1"> Rack 1</div>
+                <div class="sensor-name naeme-rack-1">Box 1</div>
                 <el-input v-model="IOLocation.Box_Sensor1" size="small" style="width:50px"></el-input>
             </div>
             <div>
-                <div class="sensor-name naeme-rack-2">Rack 2</div>
+                <div class="sensor-name naeme-rack-2">Box 2</div>
                 <el-input v-model="IOLocation.Box_Sensor2" size="small" style="width:50px"></el-input>
             </div>
         </div>
         <div class="rack-sensor-container d-flex justify-content-between px-3">
             <div>
-                <div class="sensor-name naeme-rack-1"> Rack 1</div>
+                <div class="sensor-name naeme-rack-1">Tray 1</div>
                 <el-input v-model="IOLocation.Tray_Sensor1" size="small" style="width:50px"></el-input>
             </div>
             <div>
-                <div class="sensor-name naeme-rack-2">Rack 2</div>
+                <div class="sensor-name naeme-rack-2">Tray 2</div>
                 <el-input v-model="IOLocation.Tray_Sensor2" size="small" style="width:50px"></el-input>
             </div>
         </div>
